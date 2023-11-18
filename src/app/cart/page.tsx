e0e4dbc370397b9b00a6498ba0806f15f1358a95
4754 <gaurav2k9ssk54@gmail.com>
2023-11-18 11:18:59 +0530

    Added stripe payment 	modified:   .env 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/api/(checkout)/confirm/[intentId]/route.ts 	new file:   src/app/api/(checkout)/create-intent/[orderId]/route.ts 	modified:   src/app/api/orders/route.ts 	modified:   src/app/cart/page.tsx 	new file:   src/app/pay/[id]/page.tsx 	new file:   src/app/success/page.tsx 	new file:   src/components/AddressForm.tsx 	new file:   src/components/CheckoutForm.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,18 +1,48 @@
 "use client"
 import { useCartStore } from "@/utils/store";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const CartPage = () => {
   const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+  const { data: session } = useSession();
+  const router = useRouter();
   console.log(products)
-  useEffect(()=>{
+  useEffect(() => {
     useCartStore.persist.rehydrate();
-  },[])
+  }, [])
+
+  const handleCheckout = async () => {
+    if (!session) {
+      router.push("/")
+    } else {
+      try {
+
+        const res = await fetch("http://localhost:3000/api/orders", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            price: totalPrice,
+            products,
+            status: "Not Paid",
+            userEmail: session.user.email,
+          }),
+        });
+        const data = await res.json();
+        // console.log(data);
+        router.push(`/pay/${data.id}`)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
   return (
     <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
       {/* PRODUCTS CONTAINER */}
-      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-hidden lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
         {/* SINGLE ITEM */}
         {
           products.map((item) => (
@@ -23,7 +53,7 @@ const CartPage = () => {
                 <span>{item.optionTitle}</span>
               </div>
               <h2 className="font-bold">{item.price}</h2>
-              <span className="cursor-pointer" onClick={()=> removeFromCart(item)}>X</span>
+              <span className="cursor-pointer" onClick={() => removeFromCart(item)}>X</span>
             </div>
           ))
         }
@@ -47,7 +77,7 @@ const CartPage = () => {
           <span className="">TOTAL(INCL. VAT)</span>
           <span className="font-bold">{totalPrice}</span>
         </div>
-        <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+        <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end" onClick={handleCheckout}>
           CHECKOUT
         </button>
       </div>
